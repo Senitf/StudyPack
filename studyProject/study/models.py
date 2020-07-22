cb34f2e9f3ce2fba911b9f41891ea8b35f91dbe7
Senitf <senitf1@gmail.com>
2020-07-22 16:21:29 +0900

    200722 data model fixed

--- studyProject/study/models.py
+++ studyProject/study/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 
-class content_1(models.Model):
+class Content_1(models.Model):
     def __str__(self):
         return self.file_name
     
@@ -17,8 +17,6 @@ class content_1(models.Model):
             uuid_name + extension,
         ])
 
-    photo = models.ImageField(upload_to=date_upload_to)
-
     grade = models.CharField(max_length=5)
     month = models.CharField(max_length=10)
     content_type = models.CharField(max_length=10)
@@ -26,11 +24,11 @@ class content_1(models.Model):
     category = models.CharField(max_length=10)
     content_file = models.ImageField(blank=False, null=False, upload_to=file_upload_to)
 
-class content_2(models.Model):
+class Content_2(models.Model):
     def __str__(self):
         return self.file_name
     
-    def image_upload_to(instance, filename):
+    def file_upload_to(instance, filename):
         ymd_path = timezone.now().strftime('%Y/%m/%d') 
         uuid_name = uuid4().hex
         extension = os.path.splitext(filename)[-1].lower()
@@ -38,8 +36,6 @@ class content_2(models.Model):
             ymd_path,
             uuid_name + extension,
         ])
-
-    photo = models.ImageField(upload_to=date_upload_to)
 
     publisher = models.CharField(max_length=10)
     content_label = models.CharField(max_length=10)
--- studyProject/studyProject/urls.py
+++ studyProject/studyProject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import study.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', study.views.home, name="home"),
     path('upload/', study.views.upload, name="upload"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
