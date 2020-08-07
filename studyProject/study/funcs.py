cae67327ad6a49fe5cf15ae154a579a4ee08e066
Senitf <senitf1@gmail.com>
2020-08-07 19:36:40 +0900

    data seperate final fix

--- studyProject/study/funcs.py
+++ studyProject/study/funcs.py
@@ -13,18 +13,19 @@ import os
 
 def get_file_path_1(instance, filename):
     filename = instance.year + "_" + instance.month + "_" + instance.content_grade + "_" + instance.category + "_" + instance.content_number_begin + "_" + instance.content_number_end + ".pdf"
-    return os.path.join('images/content_1', filename)
+    return os.path.join('uploads/images/content_1', filename)
 
 def get_file_path_2(instance, filename):
     filename = object_name = instance.publisher + "_" + instance.author + "_" + instance.content_label + "_" + instance.content_chapter + "_" + instance.category + "_" +  instance.content_number_begin + "_" + instance.content_number_end + ".pdf"
-    return os.path.join('images/content_2', filename)
+    return os.path.join('uploads/images/content_2', filename)
 
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-def file_division(file_name, file_index, original_file, new_path):
+def file_division(original_path, file_name, file_index, new_path):
+    item_path = os.path.join(original_path, file_name + ".pdf")
     
-    pdf = PdfFileReader(originial_file)
+    pdf = PdfFileReader(open(item_path, 'rb'))
 
     numberPages = pdf.getNumPages()
 
@@ -32,7 +33,7 @@ def file_division(file_name, file_index, original_file, new_path):
         pdf_writer = PdfFileWriter()
         pdf_writer.addPage(pdf.getPage(page))
 
-        output_name = file_name+file_index[page]+".pdf"
+        output_name = file_name+ "_" + file_index[page]+".pdf"
         save_path = os.path.join(new_path, output_name)
 
         with open(save_path, 'wb') as f:
