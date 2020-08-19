a2a3c4f79977f44e6dd8be39e213b4daf2ce14c2
Senitf <senitf1@gmail.com>
2020-08-20 07:31:35 +0900

    allowed hosts fixed

--- studyProject/studyProject/settings.py
+++ studyProject/studyProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cl*v6n_)16u5f1zc5l=u=$aff9r4!5766i9u#y0av#06r_+sgo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-hjzx8zwq.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
